6c97fd35c7b405ca8e4bbe179e98291627f619cd
Talib Aliyev <133005678+alyvswag@users.noreply.github.com>
2026-09-25 21:07:14 +0400

    feat: initialize project first commit

---
+++ src/main/java/com/example/mspin/MsPinApplication.java
@@ -0,0 +1,12 @@
+package com.example.mspin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsPinApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/mspin/MsPinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mspin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
